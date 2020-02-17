fbf1f99eea40d00064d010f251cae962eadb0ec3
v.kieffer <v.kieffer@I2020-8892>
2020-02-17 11:31:44 +0100

    Ajoutez des fichiers projet.

---
+++ Projet2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace Projet2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ImageBox = new Emgu.CV.UI.ImageBox();
+            this.ButtonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.Location = new System.Drawing.Point(87, 114);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(270, 179);
+            this.ImageBox.TabIndex = 2;
+            this.ImageBox.TabStop = false;
+            // 
+            // ButtonStart
+            // 
+            this.ButtonStart.Location = new System.Drawing.Point(87, 47);
+            this.ButtonStart.Name = "ButtonStart";
+            this.ButtonStart.Size = new System.Drawing.Size(75, 23);
+            this.ButtonStart.TabIndex = 3;
+            this.ButtonStart.Text = "Start";
+            this.ButtonStart.UseVisualStyleBackColor = true;
+            this.ButtonStart.Click += new System.EventHandler(this.ButtonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ButtonStart);
+            this.Controls.Add(this.ImageBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox ImageBox;
+        private System.Windows.Forms.Button ButtonStart;
+    }
+}
+
---
+++ Projet2/ImageProcessing.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.Util;
+
+namespace Projet2
+{
+    class ImageProcessing
+    {
+        private VideoCapture _capture = null;
+        private bool CaptureInProgress;
+        private bool RetreivedFrame = false;
+        private Mat Frame;
+        private Mat GrayFrame;
+        public Mat CannyFrame;
+
+        public ImageProcessing()
+        {
+            CvInvoke.UseOpenCL = false;
+            try
+            {
+                // Select camera
+                // 0 => first camera usually Laptop one
+                // 1 => second camera => External USB one
+                _capture = new VideoCapture(0);
+                // Set Capture property Width + Height
+                _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameWidth, 640);
+                _capture.SetCaptureProperty(Emgu.CV.CvEnum.CapProp.FrameHeight, 480);
+                // Set the function linked to ImageGrabbed event
+                _capture.ImageGrabbed += GrabProcessFrame;
+            }
+            catch (NullReferenceException excpt)
+            {
+                MessageBox.Show(excpt.Message);
+            }
+            Frame = new Mat();
+            GrayFrame = new Mat();
+            CannyFrame = new Mat();
+        }
+
+        private void GrabFrame()
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(Frame, 0);
+                // Set flag to TRUE
+                RetreivedFrame = true;
+
+            }
+        }
+
+        private void ProcessFrame()
+        {
+            if (RetreivedFrame)
+            {
+                // Convert BGR image to Gray
+                CvInvoke.CvtColor(Frame, GrayFrame, ColorConversion.Bgr2Gray);
+                // Contour finding with Canny Edge Detector filter
+                CvInvoke.Canny(GrayFrame, CannyFrame, 100, 60);
+                // Associate Images with GUI
+                //ImageBox.Image = CannyFrame;
+                // Toggle Flag
+                RetreivedFrame = !RetreivedFrame;
+            }
+
+        }
+
+        public void FindCircle()
+        {
+            Console.WriteLine(CvInvoke.FitEllipse(CannyFrame));
+        }
+
+        private void GrabProcessFrame(object sender, EventArgs arg)
+        {
+            GrabFrame();
+            ProcessFrame();
+            FindCircle();
+        }
+
+        private void ReleaseData()
+        {
+            if (_capture != null)
+                _capture.Dispose();
+        }
+    }
+}
